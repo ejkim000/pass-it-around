0ff6db814f460d3a7d96fdc69c39f0b4977ad756
ejkim000 <developer.ejk@gmail.com>
2023-08-17 17:12:09 -0400

    update

--- server.js
+++ server.js
@@ -13,19 +13,18 @@ app.get("/:number_of_bottles", (req, res) => {
   const currentBottles = req.params.number_of_bottles;
   const leftoverBottles = currentBottles - 1;
 
-  // const passItAround = (currentBottles == 0) ?
-  // `<h2><a href="/">Start Over!</a></h2>`:
-  // `<h2><a href="/${leftoverBottles}">Take one down, pass it around.</a></h2>`;
-
-  let passItAround = `<h2><a href="/${leftoverBottles}">Take one down, pass it around.</a></h2>`;
   if (currentBottles == 0) {
-    passItAround = `<h2><a href="/">Start Over!</a></h2>`;
+    res.send(`
+    <h2>${currentBottles} Bottles of soda on the wall.</h2>
+    <h2><a href="/">Start Over!</a></h2>
+    `);
+  } else {
+    res.send(`
+    <h2>${currentBottles} Bottles of soda on the wall.</h2>
+    <h2><a href="/${leftoverBottles}">Take one down, pass it around.</a></h2>
+    `);
   }
 
-  res.send(`
-    <h2>${currentBottles} Bottles of soda on the wall.</h2>
-    ${passItAround}
-    `);
 });
 
 app.listen(PORT, () => {
